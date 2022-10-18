b63b36676d8e511d02620ddc106edb2bee33e9f2
VictorObuje <vobuje@gmail.com>
2022-10-17 21:24:11 -0700

    Task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - print an excerpt from a quote
+ *
+ * Return: 1
+ */
+int main(void)
+{
+	putchar("and that piece of art is useful" - Dora Korpar, 2015-10-19");
+	return (0);
+}
+		
